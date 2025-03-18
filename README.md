b9892ef475b7f599c1155f23a538fadec265290c
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-03-18 12:25:56 +0300

    move to a separate repo

---
+++ README.md
@@ -0,0 +1,43 @@
+## Composability Stack || Smart Contracts
+
+** Smart contracts to unlock composable execution. **
+
+The composability stack solves this by allowing developers to create dynamic, multi-step transactions entirely from frontend code.
+The smart contracts in this repo handle the composable execution logic allowing developers to avoid any on-chain development and just use SDK to build composable operations.
+
+Features: 
+-   **Single-chain composability**: Use outputs of one action as inputs for another.
+For example, `swap()` method returns the amount of tokens received as a result of a swap. 
+This exact amount can be used as input for `approve()` method to allow a `stake()` method to execute.
+-   **Static types handling**: Inject any static types into the abi.encoded function call.
+-   **Several return values handling**: If function returns multiple values, you can use any amount of them as input for another function.
+-   **Constraints handling**: Validate any constraints on the input parameters.
+
+Contracts included:
+
+-   **Composable Execution Module**: ERC-7579 module, that allows Smart Accounts to execute composable transactions without changing the account implementation.
+-   **Composable Execution Base**: Base contract, that Smart Accounts can inherit from to enable composable execution natively.
+-   **Composable Execution Lib**: Library that provides methods to process input and output parameters of a composable execution.
+
+## About Composability
+[Biconomy Documentation](https://docs.biconomy.io/composability)
+
+## Usage
+
+### Install
+
+```shell
+$ pnpm i
+```
+
+### Build
+
+```shell
+$ forge build
+```
+
+### Test
+
+```shell
+$ forge test
+```
---
+++ test/ComposabilityBase.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, Vm, console2} from "forge-std/Test.sol";
+import {MockAccountFallback} from "./mock/MockAccountFallback.sol";
+import {MockAccountNonRevert} from "./mock/MockAccountNonRevert.sol";
+import {ComposableExecutionModule} from "contracts/ComposableExecutionModule.sol";
+import {MockAccountDelegateCaller} from "./mock/MockAccountDelegateCaller.sol";
+import {MockAccountCaller} from "./mock/MockAccountCaller.sol";
+import {MockAccount} from "test/mock/MockAccount.sol";
+
+address constant ENTRYPOINT_V07_ADDRESS = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+
+contract ComposabilityTestBase is Test {
+    ComposableExecutionModule internal composabilityHandler;
+    MockAccountFallback internal mockAccountFallback;
+    MockAccountDelegateCaller internal mockAccountDelegateCaller;
+    MockAccountCaller internal mockAccountCaller;
+    MockAccountNonRevert internal mockAccountNonRevert;
+    MockAccount internal mockAccount;
+
+    function setUp() public virtual {
+        composabilityHandler = new ComposableExecutionModule();
+        mockAccountFallback = new MockAccountFallback({
+            _validator: address(0),
+            _executor: address(composabilityHandler),
+            _handler: address(composabilityHandler)
+        });
+        mockAccountCaller = new MockAccountCaller({
+            _validator: address(0),
+            _executor: address(composabilityHandler),
+            _handler: address(composabilityHandler)
+        });
+        mockAccountDelegateCaller = new MockAccountDelegateCaller({
+            _composableModule: address(composabilityHandler)
+        });
+
+        vm.prank(address(mockAccountFallback));
+        composabilityHandler.onInstall(abi.encodePacked(ENTRYPOINT_V07_ADDRESS));
+
+        mockAccount = new MockAccount({_validator: address(0), _handler: address(0xa11ce)});
+        mockAccountNonRevert = new MockAccountNonRevert({_validator: address(0), _handler: address(0xa11ce)});
+
+        // fund accounts
+        vm.deal(address(mockAccountFallback), 100 ether);
+        vm.deal(address(mockAccountDelegateCaller), 100 ether);
+        vm.deal(address(mockAccountCaller), 100 ether);
+        vm.deal(address(mockAccountNonRevert), 100 ether);
+        vm.deal(address(mockAccount), 100 ether);
+    }
+}
